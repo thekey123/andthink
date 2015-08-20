a2ede2be4ad8aadcf48863acdb394dcf9785a618
thekey123 <zhengzheng@andthink.cn>
2015-08-21 02:37:40 +0800

    新建项目

---
+++ Andthink_Simple/app/src/main/java/cn/andthink/andthink_simple/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.andthink.andthink_simple;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
